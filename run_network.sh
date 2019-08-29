413e8a80811691a67a02a2e0cc774c37827cadc7
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-08-29 18:41:58 +0100

    Fixed shell type to match README instructions

--- run_network.sh
+++ run_network.sh
@@ -12,7 +12,7 @@
 #// See the License for the specific language governing permissions and
 #// limitations under the License.
 
-#!bin/sh
+#!/bin/bash
 
 echo "Press CTRL-C to stop."
 rm -rf "$PWD/pki/database.db"
